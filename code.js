c2720d31b5499c9c39dd63631be7df35fa79adf0
Jobrien0394 <84590243+Jobrien0394@users.noreply.github.com>
2021-07-22 13:05:42 -0400

    done

--- code.js
+++ code.js
@@ -2,3 +2,29 @@
 // and select "Open with Live Server"
 
 // Your Code Here.
+function snapCrackle(max) {
+    
+    let output = ""
+    
+    for (let count = 1; count <= max; count += 1) {
+        
+        let isOdd = count % 2 !== 0
+        
+        let isMultipleOf5 = count % 5 === 0
+        
+        if (isOdd === true && isMultipleOf5 === false) {
+            output += "Snap, "
+        
+        } else if (isOdd === false && isMultipleOf5 === true) {
+            output += "Crackle, "
+        
+        } else if (isOdd === true && isMultipleOf5 === true) {
+            output += "SnapCrackle, "
+
+        } else if (isOdd === false && isMultipleOf5 === false) {
+           output += count + ", "
+        }
+    }
+    return output;
+}
+document.write(snapCrackle(100))
